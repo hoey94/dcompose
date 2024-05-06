3704a185fa976e8103149230fb5eb5e6cde293a1
Demo233 <cnnqjban521@gmail.com>
2024-05-06 17:07:07 +0800

    初始化composesize项目

---
+++ main.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var url = require('url');
+var convertDockerRunToCompose = require('composerize');
+
+var port = process.argv[2]
+
+http.createServer(function(request, response){
+
+	response.writeHead(200, {'ContentType':'text/plain'});
+	// 获取参数
+	const path = url.parse(request.url, true).query;
+
+    response.write('#############################################################################################\n');
+    response.write('##                                                                                         ##\n');
+    response.write('##  Change url                                                                             ##\n');
+    response.write('##  example: http://domain?dc=docker run -d -p 8080:80 --name my-web-app nginx:latest      ##\n');
+    response.write('##                                                                                         ##\n');
+    response.write('#############################################################################################\n\n\n');
+    var res = ''
+    if (typeof(path.dc) !== 'undefined'){
+        res = 'After the conversion is:\n\n' + convertDockerRunToCompose(path.dc)
+    }
+
+	response.write(res);
+	response.write('\n');
+	response.end();
+}).listen(port);
+
+console.log('Server run at http://localhost:'+port);
